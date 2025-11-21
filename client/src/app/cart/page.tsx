aa8e5ba74ae357af66eb97f1ed08de4764f2508d
Kalpesh Parab <abhi9405.at@gmail.com>
2025-11-21 16:38:21 +0530

    first commit

--- client/src/app/cart/page.tsx
+++ client/src/app/cart/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { Suspense, useState } from 'react';
-import { CartItemsType } from '@/types';
+import { CartItemsType, ShippingFormInputs } from '@/types';
 import { useRouter, useSearchParams } from 'next/navigation';
 import { ArrowRight, Trash2 } from 'lucide-react';
 import ShippingForm from '@/components/ShippingForm';
@@ -74,7 +74,7 @@ const cartItems: CartItemsType = [
 function CartPageContent() {
   const searchParams = useSearchParams();
   const router = useRouter();
-  const [shippingForm, setShippingForm] = useState(null);
+  const [shippingForm, setShippingForm] = useState<ShippingFormInputs>();
 
   const activeStep = parseInt(searchParams.get('step') || '1');
 
@@ -152,7 +152,7 @@ function CartPageContent() {
               </div>
             ))
           ) : activeStep === 2 ? (
-              <ShippingForm setShippingForm={ setShippingForm} />
+            <ShippingForm setShippingForm={setShippingForm} />
           ) : activeStep === 3 && shippingForm ? (
             <PaymentForm />
           ) : (
--- client/src/types.ts
+++ client/src/types.ts
@@ -27,10 +27,27 @@ export const shippingFormSchema = z.object({
   phone: z
     .string()
     .min(7, 'Phone Number must be between 7 and 10 digits!')
-    .max(10 , 'Phone Number must be between 7 and 10 digits!')
+    .max(10, 'Phone Number must be between 7 and 10 digits!')
     .regex(/^\d+$/, 'Phone number must contain only numbers!'),
   address: z.string().min(1, 'Address is required!'),
   city: z.string().min(1, 'City is required!'),
 });
 
 export type ShippingFormInputs = z.infer<typeof shippingFormSchema>;
+
+export const paymentFormSchema = z.object({
+  cardHolder: z.string().min(1, 'Card holder is required!'),
+  cardNumber: z
+    .email()
+    .min(16, 'Card Number is required!')
+    .max(16, 'Card Number is required!'),
+  expirationDate: z
+    .string()
+    .regex(
+      /^(0[1-9]|1[0-2])\/\d{2}$/,
+      'Expiration Date must be in MM/YY format!'
+    ),
+  cvv: z.string().min(3, 'CVV is required!').max(3, 'CVV is required!'),
+});
+
+export type PaymentFormInputs = z.infer<typeof paymentFormSchema>;
